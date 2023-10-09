f4a29423e3120d2e04213b475c7057b38328c8d0
dinesh <dheenu02@gmail.com>
2023-10-09 10:19:32 +0530

    cucumber

--- src/main/java/stepdefinitions/LoginStep.java
+++ src/main/java/stepdefinitions/LoginStep.java
@@ -1,2 +1,28 @@
-package stepdefinitions;public class LoginStep {
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStep {
+    @Given("navigate to OrangeHRM application")
+    public void navigate_to_orange_hrm_application() {
+        System.out.println("navigate to OrangeHRM application");
+    }
+    @When("enter a valid username {string} and password {string}")
+    public void enter_a_valid_username_and_password(String username, String password) {
+        System.out.println("enter a valid username "+username+" and password "+password);
+    }
+    @When("click on the {string} button")
+    public void click_on_the_button(String login) {
+        System.out.println("click on the "+login+" button");
+    }
+    @Then("user should be navigated to dashboard")
+    public void user_should_be_navigated_to_dashboard() {
+        System.out.println("user should be navigated to dashboard");
+    }
+    @Then("user should not be navigated to dashboard")
+    public void user_should_not_be_navigated_to_dashboard() {
+        System.out.println("user should not be navigated to dashboard");
+    }
 }
